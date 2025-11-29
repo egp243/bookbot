312892c5d28f35e8c60abcf577097973bc7e933b
egp243 <egp243@gmail.com>
2025-11-29 11:45:29 -0600

    removed hardcoded path to book

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from stats import get_book_text, word_counter, char_counter, sort_characters
 import sys
 
 def main():
+    # 
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
     # Get the text from the file
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(sys.argv[1])
 
     # Calculate and print the total word count
     num_of_words = word_counter(text)
@@ -30,5 +35,7 @@ def main():
             print(f"The '{char}' character was found {count} times")
             
     print("--- End report ---")
+
+
 
 main()
