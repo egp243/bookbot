c5a350bb6e4c9034a28df69e5aa9555e0c3dcc2d
egp243 <egp243@gmail.com>
2025-11-28 21:59:22 -0600

    Implement character counting in stats.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_book_text, word_counter
+from stats import get_book_text, word_counter, char_counter
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_of_words = word_counter(text)
     print(f"Found {num_of_words} total words")
-
+    char_dict = char_counter(text)
+    print(char_dict)
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,14 @@ def get_book_text(path):
 def word_counter(text):
     count_words = text.split()
     return len(count_words)
+
+def char_counter(text):
+    char_dict = {}
+
+    for c in text:
+        lower_c = c.lower()
+        if lower_c in char_dict:
+            char_dict[lower_c] += 1
+        else:
+            char_dict[lower_c] = 1
+    return char_dict
