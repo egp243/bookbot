1cb3e86a16f4f7f1096508b0b92a30c3148980aa
egp243 <egp243@gmail.com>
2025-11-28 21:17:42 -0600

    added main() and get_book_text functions

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as book:
+        book_contents = book.read()
+        return book_contents
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
