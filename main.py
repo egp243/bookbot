2516d7d4e0aed08912fff4cc9b5f141d7f3851af
egp243 <egp243@gmail.com>
2025-11-29 11:14:40 -0600

    imported and called sorting functions to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_text, word_counter, char_counter
+from stats import get_book_text, word_counter, char_counter, sort_characters
 
 def main():
     text = get_book_text("books/frankenstein.txt")
@@ -6,4 +6,12 @@ def main():
     print(f"Found {num_of_words} total words")
     char_dict = char_counter(text)
     print(char_dict)
+    sorted_chars = sort_characters(char_dict)
+
+    for i in sorted_chars:
+        char = i["char"]
+        count = i["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+
 main()
